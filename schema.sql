8f4306b3082339750e5f9752390512d1d81190ed
Noize (or Salt) <santiagonoize@gmail.com>
2023-10-24 10:33:35 -0400

    Fix typo

--- schema.sql
+++ schema.sql
@@ -1,6 +1,6 @@
 /* Database schema to keep the structure of entire database. */
 
-CREATE table animals(
+CREATE TABLE animals(
 	id              int GENERATED ALWAYS as IDENTITY,
 	name            varchar(100) NOT NULL,
 	date_of_birth   date NOT NULL,
