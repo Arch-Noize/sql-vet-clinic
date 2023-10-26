1e718a380a0d7893dd5e7d9d6c7d68225842af38
Noize (or Salt) <santiagonoize@gmail.com>
2023-10-26 04:36:48 -0400

    Add species and owners tables

--- schema.sql
+++ schema.sql
@@ -11,3 +11,31 @@ CREATE TABLE animals(
 );
 
 ALTER TABLE animals ADD species varchar(40);
+
+CREATE TABLE owners(
+	id int GENERATED ALWAYS AS IDENTITY,
+	full_name varchar(50),
+	age int,
+	primary key(id)
+);
+
+CREATE TABLE species(
+	id int GENERATED ALWAYS AS IDENTITY,
+	name varchar(45),
+	primary key(id)
+);
+
+ALTER TABLE animals
+ADD COLUMN owner_id INT,
+ADD COLUMN species_id INT,
+DROP COLUMN species;
+
+ALTER TABLE animals
+ADD CONSTRAINT species_fk
+	FOREIGN KEY (species_id)
+		REFERENCES species(id);
+
+ALTER TABLE animals
+ADD CONSTRAINT owner_fk
+	FOREIGN KEY (owner_id)
+		REFERENCES owners(id);
