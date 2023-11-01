4cc408de6ca09bcc344acb8e8e3869bb2473adbe
Noize (or Salt) <santiagonoize@gmail.com>
2023-11-01 04:26:06 -0400

    Add index

--- data.sql
+++ data.sql
@@ -72,3 +72,10 @@ INSERT INTO visits (animal_id, vet_id, date_of_visit) VALUES
 (5, 2, '2020-08-03'),
 (10, 3, '2020-05-24'),
 (10, 1, '2021-01-11');
+
+INSERT INTO visits (animal_id, vet_id, date_of_visit) SELECT * FROM (SELECT id FROM animals) animal_ids, (SELECT id FROM vets) vets_ids, generate_series('1980-01-01'::timestamp, '2021-01-01', '4 hours') visit_timestamp;
+INSERT into owners (full_name, email) select 'Owner ' || generate_series(1,2500000), 'owner_' || generate_series(1,2500000) || '@mail.com';
+
+CREATE INDEX animal_index ON visits (animal_id);
+CREATE INDEX vet_index ON visits (vet_id);
+CREATE INDEX email_index ON owners (email);
